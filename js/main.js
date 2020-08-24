eb86367b28fb0eb8ac642340f8b704261599b05a
WeatGame <66261148+WeatGame@users.noreply.github.com>
2020-08-24 20:36:49 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+
+	$('.slider_inner').slick({
+ 		nextArrow: '<button type="button" class = "slick-button slick-next">  </button>',
+ 		prevArrow: '<button type="button" class = "slick-button slick-prev">  </button>',
+		infinite:false
+	});
+
+
+
+
+});
